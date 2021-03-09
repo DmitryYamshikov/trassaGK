5ac646f9d46da0234168d8d717dda7d1ebc43f8d
Dmitry <Yamshikov>
2021-03-09 17:57:51 +0700

    сделан адаптив главной, сделана страница заказа карт

--- src/js/main.js
+++ src/js/main.js
@@ -51,6 +51,11 @@ window.addEventListener("DOMContentLoaded", () => {
         if (!e.target.closest('.phone-togler') && !e.target.closest('.header__contacts-mobile-more')) {
             phoneMore.classList.remove('active');
         }
+        if (!e.target.closest('.form_modal') && !e.target.closest('[data-modal]')) {
+            document.querySelector('.modal').classList.remove('active');
+            document.body.style.overflow = '';
+            document.body.style.paddingRight = '0px';
+        }
     });
 
 
@@ -69,9 +74,65 @@ window.addEventListener("DOMContentLoaded", () => {
         infinite: true,
         prevArrow: '<button class="slider-arrow slider-arrow_prev"></button>',
         nextArrow: '<button class="slider-arrow slider-arrow_next"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            /* {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            } */
+        ]
+    });
+    //акции на главной
+    if (document.documentElement.clientWidth < 769) {
+        $('.index-page .events_sale .events__wrapper').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            prevArrow: '<button class="slider-arrow slider-arrow_prev"></button>',
+            nextArrow: '<button class="slider-arrow slider-arrow_next"></button>',
+        });
+    }
+    //открытие модалки
+    const btnCallModal = document.querySelector('[data-modal]');
+    btnCallModal.addEventListener('click', function () {
+        const modal = document.querySelector('.modal');
+        document.body.style.overflow = 'hidden';
+        document.body.style.paddingRight = '15px';
+        modal.classList.add('active');
     });
 
+    //tabs на странице заказа карт
 
+    const btnsTabAnkete = document.querySelectorAll('.ankete__tab-togler .btn');
+    if (btnsTabAnkete) {
+        const anketeTabs = document.querySelectorAll('.ankete__tabs .form');
+        btnsTabAnkete.forEach((item,i) => {
+            item.addEventListener('click', ()=>{
+                btnsTabAnkete.forEach(item2=>{
+                    item2.classList.remove('active');
+                });
+                item.classList.add('active');
+                anketeTabs.forEach(itemTabs=>itemTabs.classList.remove('active'));
+                anketeTabs[i].classList.add('active');
+            });
+        });
+    }
 });
 
 $(document).ready(function () {
